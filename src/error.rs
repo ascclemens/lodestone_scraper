519cbe719fe4247b94050015a3a9fc25c6d4a043
Kyle Clemens <git@kyleclemens.com>
2018-09-05 09:56:57 -0400

    refactor: lowercase lodestone

--- src/error.rs
+++ src/error.rs
@@ -6,7 +6,7 @@ pub type Result<T> = std::result::Result<T, Error>;
 pub enum Error {
   #[fail(display = "not found")]
   NotFound,
-  #[fail(display = "Lodestone responded with an unexpected code: {}", _0)]
+  #[fail(display = "lodestone responded with an unexpected code: {}", _0)]
   UnexpectedResponse(StatusCode),
   #[fail(display = "network error: {}", _0)]
   Net(reqwest::Error),
